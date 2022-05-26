88bb098428a1b0b740a083fd48bed695c1fe5704
caffeinejun <gurwns232e@gmail.com>
2022-05-26 16:58:26 +0900

    deleteTodo

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ const $input = document.querySelector('.new-task > input')
 const $todos = document.querySelector('#todos')
 
 const printTodos = () => {
-    const html = todos.map(todo => {
+    const html = todos.map((todo, index) => {
         return `
-		<li>
+		<li data-index="${index}">
 			<button class="delete">×</button>
 			<input type="checkbox" class="toggle-checked" />
 			<span class="text">${todo.title}</span>
@@ -28,31 +28,42 @@ const checkTodo = event => {
     event.preventDefault()
     console.log($input.value)
 
+    // todo 추가
     const todo = {title: $input.value, isDone: false}
     todos.push(todo)
     console.log(todos)
 
+    // input 초기화
     $input.value = ''
 
+    // todos 출력
     printTodos()
 }
 
+//
+// 폼,input 입력 이벤트 추가하기
+// 입력했을때 todo추가
 $form.addEventListener('submit', checkTodo)
 
-const deleteTodo = () => {
-    console.log('delete')
+const deleteTodo = index => {
+    console.log('delete', index)
+    // todos에서 index번째 todo 삭제
+    todos.splice(index, 1)
+    printTodos()
 }
-
 const toggleTodo = () => {
     console.log('toggle')
 }
 
+// 삭제
 document.body.addEventListener('click', event => {
+    const index = parseInt(event.target.parentNode.dataset.index, 10)
     if (event.target.className === 'delete') {
-        deleteTodo()
+        deleteTodo(index)
     } else if (event.target.className === 'toggle-checked') {
-        toggleTodo()
+        toggleTodo(index)
     }
 })
 
+// todos 출력
 printTodos()
